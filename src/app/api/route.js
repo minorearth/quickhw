7c82f65cf4c6c4dc051c0551a82792ed76fe3ccb
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-08-27 00:31:25 +0300

    date fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { addDocInCollection } from "../../datamodel";
 
 export async function GET() {
-  const today = new Date();
+  const today = new Date(2011, 10, 30, 12, 1, 1);
   const data = { title: "Новый опрос16", datetime: today, user: "Попкин3" };
   addDocInCollection("surveys", { ...data });
   return NextResponse.json({ fuck: "hello" });
