27ee734523de0c0c86790faf1df0706993405681
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-08-27 00:23:08 +0300

    cron test

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,7 +3,7 @@ import { addDocInCollection } from "../../datamodel";
 
 export async function GET() {
   const today = new Date();
-  const data = { title: "Новый опрос16", datetime: today, user: "Попкин2" };
+  const data = { title: "Новый опрос16", datetime: today, user: "Попкин3" };
   addDocInCollection("surveys", { ...data });
   return NextResponse.json({ fuck: "hello" });
 }
