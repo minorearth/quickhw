c7f79fb6e9d72edbf34192fe60a2fa500980c5a0
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-08-27 14:23:18 +0300

    anothe api fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,10 +2,13 @@ import { NextResponse } from "next/server";
 import { addDocInCollection } from "../../datamodel";
 import { deleteAllDocsInCollection } from "../../datamodelSSR";
 
+export const dynamic = "force-dynamic";
+// https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
+
 export async function GET() {
   // const today = new Date(2011, 10, 30, 12, 1, 2);
   // const data = { title: "Новый опрос16", datetime: today, user: "Попкин3" };
   // addDocInCollection("surveys", { ...data });
-  const res = await deleteAllDocsInCollection("surveys", -10);
+  const res = deleteAllDocsInCollection("surveys", -10);
   return NextResponse.json(res);
 }
