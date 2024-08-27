3f427dab1e75bc955edbcec7fc5299350f2f02a9
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-08-27 16:12:33 +0300

    anothe api fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,56 +1,5 @@
 import { NextResponse } from "next/server";
-
-import {
-  collection,
-  getDocs,
-  doc,
-  deleteDoc,
-  initializeFirestore,
-  Timestamp,
-} from "firebase/firestore";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0; //revalidate api every 1 second
-// https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBwMnO7HaGuHu6LrzsTj6y6J9BojyC1ei0",
-  authDomain: "testchallenge-52d1b.firebaseapp.com",
-  projectId: "testchallenge-52d1b",
-  storageBucket: "testchallenge-52d1b.appspot.com",
-  messagingSenderId: "785621858975",
-  appId: "1:785621858975:web:e1fcef81ff499466bd40aa",
-  measurementId: "G-E08Z0JNFH2",
-};
-
-import { initializeApp } from "firebase/app";
 import { deleteAllDocsInCollection } from "../../datamodelSSR";
-
-const app = initializeApp(firebaseConfig);
-
-const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-  useFetchStreams: false,
-});
-
-// export const deleteAllDocsInCollection = async (collectionName, timeLag) => {
-//   let th = new Date();
-//   th.setDate(th.getDate() - timeLag);
-//   const thresold = Timestamp.fromDate(th);
-//   const col = collection(db, collectionName);
-//   const q = query(col, where("datetime", "<=", thresold));
-//   const docs = await getDocs(q);
-//   // const docs = await getDocs(col);
-//   let log = { len: docs.docs.length, date: th.toString(), vers: "4" };
-//   await Promise.all(
-//     docs.docs.map(async (docS) => {
-//       deleteAllFileFromDir(`/capture/${docS.id}`);
-//       log = { ...log, [docS.id]: docS.id };
-//       await deleteDoc(doc(db, collectionName, docS.id));
-//     })
-//   );
-//   return log;
-// };
 
 export async function GET() {
   const res = await deleteAllDocsInCollection("surveys", -10);
