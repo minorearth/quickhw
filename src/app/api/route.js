246538c21d61aff28c002068325bcd573a01ac03
Ivan Kaponin <dayfireacad@Ivans-MacBook-Pro.local>
2024-08-27 16:17:15 +0300

    anothe api fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,37 @@
 import { NextResponse } from "next/server";
-import { deleteAllDocsInCollection } from "../../datamodelSSR";
+
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  initializeFirestore,
+  Timestamp,
+} from "firebase/firestore";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0; //revalidate api every 1 second
+// https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBwMnO7HaGuHu6LrzsTj6y6J9BojyC1ei0",
+//   authDomain: "testchallenge-52d1b.firebaseapp.com",
+//   projectId: "testchallenge-52d1b",
+//   storageBucket: "testchallenge-52d1b.appspot.com",
+//   messagingSenderId: "785621858975",
+//   appId: "1:785621858975:web:e1fcef81ff499466bd40aa",
+//   measurementId: "G-E08Z0JNFH2",
+// };
+
+// import { initializeApp } from "firebase/app";
+// import { deleteAllDocsInCollection } from "../../datamodelSSR";
+
+// const app = initializeApp(firebaseConfig);
+
+// const db = initializeFirestore(app, {
+//   experimentalForceLongPolling: true,
+//   useFetchStreams: false,
+// });
 
 export async function GET() {
   const res = await deleteAllDocsInCollection("surveys", -10);
